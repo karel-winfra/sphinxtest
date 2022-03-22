2c3985b2fc3aa2b7881334c4a9620f98fd1743ae
Karel <101720746+karel-winfra@users.noreply.github.com>
2022-03-22 12:15:30 +0100

    livereload tiene en cuenta toda la estructura

--- runserver.py
+++ runserver.py
@@ -1,10 +1,21 @@
+import os
 from livereload import Server, shell
+
+carpetas = [
+    "puesta_en_marcha",
+    "winfra_cliente"
+]
+
 
 if __name__ == '__main__':
     server = Server()
-    server.watch('winfra_cliente/winfra_ui/*.md', shell('make.bat html'), delay=1)
-    server.watch('*.md', shell('make.bat html'), delay=1)
+
+    for carpeta_raiz in carpetas:
+        for root, carpeta, fichero in os.walk(carpeta_raiz):
+            server.watch(os.path.join(root, '*.md'), shell('make.bat html'), delay=1)
+
     server.watch('*.py', shell('make.bat html'), delay=1)
+
     server.watch('_static/*', shell('make.bat html'), delay=1)
     server.watch('_templates/*', shell('make.bat html'), delay=1)
     server.serve(root='_build/html')
